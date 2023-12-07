eeeb4fbb5ee7c3375effad29036a442cbc72befd
TaeJeong <ksmpjc99@gmail.com>
2023-12-07 18:32:33 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Project3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_new_save = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(12, 11);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(123, 34);
+            this.btn_load.TabIndex = 1;
+            this.btn_load.Text = "불러오기";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(141, 12);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(123, 34);
+            this.btn_save.TabIndex = 2;
+            this.btn_save.Text = "저장";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_new_save
+            // 
+            this.btn_new_save.Location = new System.Drawing.Point(270, 12);
+            this.btn_new_save.Name = "btn_new_save";
+            this.btn_new_save.Size = new System.Drawing.Size(123, 34);
+            this.btn_new_save.TabIndex = 3;
+            this.btn_new_save.Text = "다른 이름으로 저장";
+            this.btn_new_save.UseVisualStyleBackColor = true;
+            this.btn_new_save.Click += new System.EventHandler(this.btn_new_save_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 576);
+            this.Controls.Add(this.btn_new_save);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_new_save;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int XSIZE = 512;
+        int YSIZE = 512;
+        byte[,] inImage;
+
+        Bitmap bitmap;
+
+        void LoadImage(string fileName)
+        {
+            BinaryReader fp = new BinaryReader(File.Open(fileName, FileMode.Open));
+            inImage = new byte[XSIZE, YSIZE];
+
+            for (int i = 0; i < XSIZE; i++)
+            {
+                for (int j = 0; j < YSIZE; j++)
+                {
+                    inImage[i, j] = fp.ReadByte();  // 한 바이트씩 읽어서 메모리에 로드
+                }
+            }
+            fp.Close();
+        }
+
+        /*
+         * 흑백 이미지
+         */
+        void DisplayImage()
+        {
+            for (int i = 0; i < XSIZE; i++)
+            {
+                for (int j = 0; j < YSIZE; j++)
+                {
+                    byte data = inImage[j, i];
+                    Color c = Color.FromArgb(data, data, data);
+                    bitmap.SetPixel(i, j, c);
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        string filePath;
+
+        /*
+         * 흑백 이미지 로드
+         */
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "이미지 파일|*.raw;|모든 파일|*.*";
+                // openFileDialog.Filter = "이미지 파일|*.raw;*.png;*.jpg;*.jpeg;*.gif;*.bmp|모든 파일|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 선택한 파일의 경로 가져오기
+                    filePath = openFileDialog.FileName;
+
+                    bitmap = new Bitmap(XSIZE, YSIZE);
+
+                    LoadImage(filePath);
+                    DisplayImage();
+                }
+            }
+        }
+
+        /*
+         * 컬러 이미지 로드
+         */
+        /*private void btn_load_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "이미지 파일|*.png;*.jpg;*.jpeg;*.gif;*.bmp|모든 파일|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    // 이미지를 PictureBox에 표시
+                    loadedImage = Image.FromFile(filePath);
+                    pictureBox1.Image = loadedImage;
+                }
+            }
+        }*/
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            // PictureBox의 이미지를 저장
+            SaveImage(filePath);
+        }
+
+        private void btn_new_save_Click(object sender, EventArgs e)
+        {
+            if (filePath != null)
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "이미지 파일|*.raw;|모든 파일|*.*";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        filePath = saveFileDialog.FileName;
+
+                        // PictureBox의 이미지를 저장
+                        SaveImage(filePath);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("이미지가 로드되지 않았습니다.\n먼저 이미지를 열어주세요.");
+            }
+        }
+
+        private void SaveImage(string filePath)
+        {
+            if(filePath != null)
+            {
+                try
+                {
+                    // PictureBox의 이미지를 파일로 저장
+                    pictureBox1.Image.Save(filePath);
+
+                    MessageBox.Show("이미지가 성공적으로 저장되었습니다.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("이미지를 저장하는 중에 오류가 발생했습니다. 오류 메시지: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("이미지가 로드되지 않았습니다.\n먼저 이미지를 열어주세요.");
+            }
+        }
+
+        private bool drawing = false;
+        private Point startPoint;
+        private Point endPoint;
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawing = true;
+            startPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                endPoint = e.Location;
+
+                // PictureBox에 선을 그림
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                {
+                    using (Pen pen = new Pen(Color.Black, 2))
+                    {
+                        g.DrawLine(pen, startPoint, endPoint);
+                    }
+                }
+
+                startPoint = endPoint;
+                pictureBox1.Invalidate(); // PictureBox를 다시 그리도록 갱신
+            }
+        }
+
+        /*int x1, y1, x2, y2;
+        Pen pen = new Pen(Color.Black, 1);
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            x2 = e.X;
+            y2 = e.Y;
+
+            Graphics g = CreateGraphics();
+            g.DrawLine(pen, x1, y1, x2, y2);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x1 = e.X;
+            y1 = e.Y;
+        }*/
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*this.ClientSize = new System.Drawing.Size(XSIZE, YSIZE);    // 사이즈에 맞게 폼 생성
+
+            pictureBox1.Location = new System.Drawing.Point(0, 0);      // 픽처박스의 좌표 설정
+            pictureBox1.Size = new System.Drawing.Size(XSIZE, YSIZE);   // 픽처박스의 사이즈 설정*/
+        }
+
+    }
+}
